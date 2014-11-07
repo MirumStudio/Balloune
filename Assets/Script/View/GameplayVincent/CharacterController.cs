39f423f78f947de1754c81b5e7631dc771d87283
Vincent Bernier <vince14_evil@hotmail.com>
2014-11-07 10:10:37 -0500

    Implement MainMenu and begin work

---
+++ Assets/Script/View/GameplayVincent/CharacterController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(-(Vector2.right * Time.deltaTime));
+        }
+	}
+}
