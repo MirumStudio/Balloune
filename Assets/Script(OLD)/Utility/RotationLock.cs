efdd9786a2fb5f5703988e78a34cc434be9f5518
Vincent Bernier <vince14_evil@hotmail.com>
2014-12-10 17:38:26 -0500

    clean

---
+++ Assets/Script(OLD)/Utility/RotationLock.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationLock : MonoBehaviour {
+
+	[SerializeField]
+	private Vector3 m_Rotation;
+	private Quaternion mLockedRotation;
+	void Start()
+	{
+		this.mLockedRotation=Quaternion.Euler(this.m_Rotation);
+	}
+	void Update () 
+	{
+		this.transform.rotation=this.mLockedRotation;
+	}
+}
