f7b2c8d0742559f220b4d24eac76f2f0f050c24c
Léandre M.M. <Leandre_Manseau_Martineau@hotmail.com>
2014-12-11 13:02:35 -0500

    AutoFollow + GrayScale Shader
    Autofollow Script for Camera. Sprites/Grayscale : New Shader using a
    Grayscale slider, compatible with sprite renderer.

---
+++ Assets/Script(OLD)/Utility/AutoFollow.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// This script allow smooth following of the attached object to the target object on selected axis. On a camera to the main character for example.
+/// </summary>
+public class AutoFollow : MonoBehaviour {
+
+	[SerializeField]
+	private Transform m_Target;
+	[SerializeField]
+	private float m_Smoothness = 0.5f; //Should be between 0<Value<=1. 1 means a following without smoothness. Lower than 1 mean a delayed smoothing.
+	[SerializeField]
+	private bool m_FollowX = false;
+	[SerializeField]
+	private bool m_FollowY = false;
+	[SerializeField]
+	private bool m_FollowZ = false;
+
+	void Start ()
+	{
+		if(this.m_Target==null)
+		{
+			Debug.LogError("No target is assigned to the AutoFollow : desactivating the script");
+			this.enabled=false;
+		}
+		if(m_Smoothness>1)
+		{
+			Debug.LogWarning("The smoothness value of AutoFollow is greater than 1, this will behave as if it was 1.");
+			this.m_Smoothness=1;
+		}
+		else if(m_Smoothness<=0)
+		{
+			Debug.LogWarning("The smoothness value of AutoFollow is zero or lower. Consider desactivating the script instead.");
+		}
+		if(!(m_FollowX||m_FollowY||m_FollowZ))
+		{
+			Debug.LogWarning("The AutoFollow do not use any axis, consider desactivating the script.");
+		}
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 target = this.transform.position;
+		if(this.m_FollowX)
+		{
+			target.x = this.m_Target.position.x;
+		}
+		if(this.m_FollowY)
+		{
+			target.y = this.m_Target.position.y;
+		}
+		if(this.m_FollowZ)
+		{
+			target.z = this.m_Target.position.z;
+		}
+		this.transform.position=Vector3.Lerp(this.transform.position,target,this.m_Smoothness);
+	}
+}
