b4bb6084af4361e583e5df7eae37fad1626b6a33
Vincent Bernier <vince14_evil@hotmail.com>
2014-12-21 11:34:07 -0500

    Try to put balloun in main menu

--- Assets/Script(OLD)/View/GamePlayLeandre/BaloonBehavior.cs
+++ Assets/Script(OLD)/View/GamePlayLeandre/BaloonBehavior.cs
@@ -8,16 +8,15 @@ public class BaloonBehavior : MonoBehaviour {
 	private SpringJoint2D m_SpringJoint;
 	[SerializeField]
 	private Transform m_RopeStart;
-
 	void Start()
 	{
-		this.m_LineRenderer.sortingLayerName="Background";
-		this.m_LineRenderer.sortingOrder=0;
+		/*this.m_LineRenderer.sortingLayerName="Background";
+		this.m_LineRenderer.sortingOrder=0;*/
 	}
 	void Update()
 	{
 		this.m_LineRenderer.SetPosition(0,this.m_RopeStart.transform.position);
-		this.m_LineRenderer.SetPosition(1,this.m_SpringJoint.connectedBody.transform.position);
+		//this.m_LineRenderer.SetPosition(1,this.m_SpringJoint.connectedBody.transform.position);
 	}
 	void OnMouseOver()
 	{
