62935e2657c79090840df53c19a73350bae8c2a2
Vincent Bernier <vince14_evil@hotmail.com>
2015-01-01 12:06:58 -0500

    Create test script to control greyScale

---
+++ Assets/Scene/Gameplay/Level2/GreyScaleTestScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GreyScaleTestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        renderer.material.SetFloat("_GrayScl", 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
