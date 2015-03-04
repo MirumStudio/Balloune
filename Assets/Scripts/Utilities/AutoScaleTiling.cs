7fc186c68298610105b7989805d008c42324666a
Vincent Bernier <vince14_evil@hotmail.com>
2015-03-04 07:14:20 -0500

    tr

--- Assets/Scripts/Utilities/AutoScaleTiling.cs
+++ Assets/Scripts/Utilities/AutoScaleTiling.cs
@@ -7,7 +7,7 @@ public class AutoScaleTiling : MonoBehaviour {
 
 	void Start () 
 	{
-		this.GetComponent<Renderer>().material.mainTextureScale=new Vector2(this.transform.localScale.x*m_TileRatio.x,this.transform.localScale.z*m_TileRatio.y);
+		this.renderer.material.mainTextureScale=new Vector2(this.transform.localScale.x*m_TileRatio.x,this.transform.localScale.z*m_TileRatio.y);
 		Destroy(this);
 	}
 
