934abba0fc1847bd3a1b0b7f36704c0b5d02f93e
CourteousCrook <pocorbin@gmail.com>
2015-03-31 23:48:27 +0200

    Created Enemy File in resources and now have an enemy walking from wall to wall

--- Assets/Scripts/Gameplay/Character/CharacterAnimator.cs
+++ Assets/Scripts/Gameplay/Character/CharacterAnimator.cs
@@ -1,49 +1,54 @@
 ﻿using UnityEngine;
 
 public class CharacterAnimator : MonoBehaviour {
-
-    private const string ANIMATION_PARAM_DIRECTION_NAME = "Direction";
+	
+	private const string ANIMATION_PARAM_DIRECTION_NAME = "Direction";
 	private const string ANIMATION_PARAM_SPEED_NAME = "Speed";
-    private const int ANIMATION_LEFT_DIRECTION = 0;
-    private const int ANIMATION_RIGHT_DIRECTION = 1;
-
-    [SerializeField]
-    private float m_WalkAnimationSpeed = 1f;
-    [SerializeField]
-    private float m_RunAnimationSpeed = 2f;
-
-    private Animator mAnimator;
-
+	private const int ANIMATION_LEFT_DIRECTION = 0;
+	private const int ANIMATION_RIGHT_DIRECTION = 1;
+	
+	[SerializeField]
+	private float m_WalkAnimationSpeed = 1f;
+	[SerializeField]
+	private float m_RunAnimationSpeed = 2f;
+	
+	private Animator mAnimator;
+	
 	void Start () {
-        mAnimator = GetComponent<Animator>();
-        SetAnimationDirection(ANIMATION_RIGHT_DIRECTION);
+		mAnimator = GetComponent<Animator>();
+		SetAnimationDirection(ANIMATION_RIGHT_DIRECTION);
 	}
-
-    public void UpdateAnimation(Direction pDirection, bool pIsGrounded, bool mIsRunning)
-    {
-        if (pDirection.Edge == EEdge.NONE)
-        {
-            StopAnimation();
-        }
-        else
-        {
-            UpdateAnimationDirection(pDirection, pIsGrounded);
+	
+	public void UpdateAnimation(Direction pDirection, bool pIsGrounded, bool mIsRunning)
+	{
+		if (pDirection.Edge == EEdge.NONE)
+		{
+			StopAnimation();
+		}
+		else
+		{
+			UpdateAnimationDirection(pDirection, pIsGrounded);
 			UpdateAnimationSpeed(mIsRunning);
-        }
-    }
-
-    private void StopAnimation()
-    {
-        mAnimator.speed = 0;
-    }
-
-    #region AnimationDirection
-    private void UpdateAnimationDirection(Direction pDirection, bool pIsGrounded)
-    {
+		}
+	}
+	
+	public void UpdateAnimation(Direction pDirection, bool pIsGrounded)
+	{
+		UpdateAnimation (pDirection, pIsGrounded, false);
+	}
+	
+	private void StopAnimation()
+	{
+		mAnimator.speed = 0;
+	}
+	
+	#region AnimationDirection
+	private void UpdateAnimationDirection(Direction pDirection, bool pIsGrounded)
+	{
 		//Instead, we use flipping in BaseCharacterController
 		//This function is keep for reference
-        return;
-        if (pIsGrounded)
+		return;
+		/*if (pIsGrounded)
         {
             if (pDirection.IsLeftDirection())
             {
@@ -57,39 +62,39 @@ public class CharacterAnimator : MonoBehaviour {
         else
         {
             //JUMP ANIMATION
-        }
-    }
-
-    private void SetAnimationDirection(int pAnimationDirection)
-    {
-        mAnimator.SetInteger(ANIMATION_PARAM_DIRECTION_NAME, pAnimationDirection);
-    }
-    #endregion
-
-    #region AnimationSpeed
-    private void UpdateAnimationSpeed(bool mIsRunning)
-    {
+        }*/
+	}
+	
+	private void SetAnimationDirection(int pAnimationDirection)
+	{
+		mAnimator.SetInteger(ANIMATION_PARAM_DIRECTION_NAME, pAnimationDirection);
+	}
+	#endregion
+	
+	#region AnimationSpeed
+	private void UpdateAnimationSpeed(bool mIsRunning)
+	{
 		float speed = 0;
-        if (mIsRunning)
-        {
+		if (mIsRunning)
+		{
 			speed=1;
-            SetRunAnimationSpeed();
-        }
-        else
-        {
-            SetWalkAnimationSpeed();
-        }
+			SetRunAnimationSpeed();
+		}
+		else
+		{
+			SetWalkAnimationSpeed();
+		}
 		mAnimator.SetFloat(ANIMATION_PARAM_SPEED_NAME,speed);
-    }
-
-    private void SetWalkAnimationSpeed()
-    {
-        mAnimator.speed = m_WalkAnimationSpeed;
-    }
-
-    private void SetRunAnimationSpeed()
-    {
-        mAnimator.speed = m_RunAnimationSpeed;
-    }
-#endregion
+	}
+	
+	private void SetWalkAnimationSpeed()
+	{
+		mAnimator.speed = m_WalkAnimationSpeed;
+	}
+	
+	private void SetRunAnimationSpeed()
+	{
+		mAnimator.speed = m_RunAnimationSpeed;
+	}
+	#endregion
 }
--- Assets/Scripts/Gameplay/Character/MainCharacterController.cs
+++ Assets/Scripts/Gameplay/Character/MainCharacterController.cs
@@ -4,13 +4,13 @@ using Radix.Event;
 
 [RequireComponent (typeof(CharacterAnimator))]
 public class MainCharacterController : BaseCharacterController {
-  
-    public GameObject RightButton;
-    public GameObject LeftButton;
-    public GameObject JumpButton;
-    //public GameObject RunButton;
-
-    [SerializeField]
+	
+	public GameObject RightButton;
+	public GameObject LeftButton;
+	public GameObject JumpButton;
+	//public GameObject RunButton;
+	
+	[SerializeField]
 	private float m_RunSpeed = 10;
 	[SerializeField]
 	private float m_BoostedJumpForce = 300f;
@@ -18,14 +18,14 @@ public class MainCharacterController : BaseCharacterController {
 	private bool mCanBoostJump = false;
 	private float mTimePressed = 0;
 	
-    private CharacterAnimator mAnimator;
-
+	private CharacterAnimator mAnimator;
+	
 	public override void Start ()
 	{
 		base.Start ();
 		mAnimator=GetComponent<CharacterAnimator>();
 	}
-		
+	
 	protected override void Update (){
 		base.Update ();
 		
@@ -35,12 +35,17 @@ public class MainCharacterController : BaseCharacterController {
 			mTimePressed = 0;
 		}
 	}
-
+	
+	protected override void FixedUpdate() 
+	{
+		base.FixedUpdate ();
+	}
+	
 	protected override bool CharacterWantToJump 
 	{
-        get { return Input.GetKey(KeyCode.Space) || (JumpButton != null && JumpButton.GetComponent<ButtonOnPressed>().IsPressed);}
+		get { return Input.GetKey(KeyCode.Space) || (JumpButton != null && JumpButton.GetComponent<ButtonOnPressed>().IsPressed);}
 	}
-
+	
 	protected override void UpdateJumping()
 	{
 		mTimePressed = mTimePressed + Time.deltaTime;
@@ -48,7 +53,7 @@ public class MainCharacterController : BaseCharacterController {
 			jump ();
 		}
 	}
-
+	
 	private void jump(){
 		if (base.mInitJumping)
 		{
@@ -60,71 +65,71 @@ public class MainCharacterController : BaseCharacterController {
 			boostJump ();
 		}
 	}
-
+	
 	private void boostJump(){
 		//Character is already in the air
 		mRigidbody2D.AddForce(new Vector2(0f, m_BoostedJumpForce));
 		mTimePressed = 0;
 		mCanBoostJump = false;
 	}
-
+	
 	protected override int GetHorizontalAxisValue() 
 	{
-        float value = Input.GetAxis("Horizontal");
-
-        if(value == 0)
-        {
-            if(RightButton != null && RightButton.GetComponent<ButtonOnPressed>().IsPressed)
-            {
-                value++;
-            }
-            else if (LeftButton != null && LeftButton.GetComponent<ButtonOnPressed>().IsPressed)
-            {
-                value--;
-            }
-            return (int)value;
-        }
-
-        return (int)Mathf.Sign(value);
+		float value = Input.GetAxis("Horizontal");
+		
+		if(value == 0)
+		{
+			if(RightButton != null && RightButton.GetComponent<ButtonOnPressed>().IsPressed)
+			{
+				value++;
+			}
+			else if (LeftButton != null && LeftButton.GetComponent<ButtonOnPressed>().IsPressed)
+			{
+				value--;
+			}
+			return (int)value;
+		}
+		
+		return (int)Mathf.Sign(value);
 	}
-
-    private bool PlayerWantToRun
-    {
-        get { return Input.GetKey(KeyCode.LeftShift)/* || RunButton.GetComponent<ButtonOnPressed>().IsPressed*/; }
-    }
-    
+	
+	private bool PlayerWantToRun
+	{
+		get { return Input.GetKey(KeyCode.LeftShift)/* || RunButton.GetComponent<ButtonOnPressed>().IsPressed*/; }
+	}
+	
 	/*protected override float GetSpeed()
     {
         return PlayerWantToRun ? m_RunSpeed : WalkSpeed;
     }*/
 	
-    private bool PlayerWantToJump
-    {
-        get { return Input.GetKey(KeyCode.Space); }
-    }
-
-    protected override void UpdateAnimation(Direction pDirection, bool pIsGrounded)
+	private bool PlayerWantToJump
+	{
+		get { return Input.GetKey(KeyCode.Space); }
+	}
+	
+	protected override void UpdateAnimation(Direction pDirection, bool pIsGrounded)
 	{
 		this.mAnimator.UpdateAnimation(pDirection,pIsGrounded,PlayerWantToRun);
 	}
-
-    public void OnCollisionEnter2D(Collision2D pCollision)
-    {
-        var interactable = pCollision.gameObject.GetComponent<Interactable>();
-        if (interactable != null)
-        {
-            interactable.DispacthEvent();
-            if (interactable.IsPassableThrough)
-            {
-                var interactableCollider = pCollision.gameObject.GetComponent<Collider2D>();
-                Physics2D.IgnoreCollision(GetComponent<Collider2D>(), interactableCollider);
-            }
-        }
-    }
-
-    /* FOR REFERENCE
+	
+	public void OnCollisionEnter2D(Collision2D pCollision)
+	{
+		var interactable = pCollision.gameObject.GetComponent<Interactable>();
+		if (interactable != null)
+		{
+			interactable.DispacthEvent();
+			if (interactable.IsPassableThrough)
+			{
+				var interactableCollider = pCollision.gameObject.GetComponent<Collider2D>();
+				Physics2D.IgnoreCollision(GetComponent<Collider2D>(), interactableCollider);
+			}
+		}
+	}
+	
+	/* FOR REFERENCE
 	How reset ignoreCollision
      * /*pCollision.gameObject.SetActive(false);
             pCollision.gameObject.SetActive(true);*/
-    //pCollision.gameObject.SetActive(false);*/
+	//pCollision.gameObject.SetActive(false);*/
 }
