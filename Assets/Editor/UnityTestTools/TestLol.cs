7b8e2f00362f2730e0eac5a46b51d0ef7ce6940a
Vincent Bernier <vince14_evil@hotmail.com>
2015-05-05 11:49:40 -0400

    Add Unity Test Tools

---
+++ Assets/Editor/UnityTestTools/TestLol.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using NUnit.Framework;
+public class TestLol : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    [Test]
+    public void lol()
+    {
+        Assert.Pass();
+        Assert.Pass();
+    }
+}
