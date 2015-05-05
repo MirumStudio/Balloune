d0c15a77e69a3a2d93b9c5ffd4bbd84a9ead65bd
CourteousCrook <pocorbin@gmail.com>
2015-05-05 17:46:04 +0200

    Changing the number of Hinges (in the balloonHolder) and/or the balloon's distance from the character now properly creates a rope

--- Assets/Scripts/Gameplay/BalloonHolder.cs
+++ Assets/Scripts/Gameplay/BalloonHolder.cs
@@ -13,7 +13,7 @@ public class BalloonHolder : MonoBehaviour {
 	protected GameObject m_PrefabRope;
 
 	[SerializeField]
-	int ropeLength = 4;
+	int m_NumberOfHinges = 4;
 
 	private GameObject[] m_LifeBalloons = new GameObject[3];
 	private int heldBalloons = 0;
@@ -34,17 +34,21 @@ public class BalloonHolder : MonoBehaviour {
 	{
 		//Rope rope = new Rope(ropeLength);
 		//attach rope to character
-		GameObject ropeGameObject = Instantiate (m_PrefabRope, new Vector2 (x, 3), Quaternion.identity) as GameObject;
-		Rope rope = ropeGameObject.GetComponent<Rope> ();
-		rope.createRope (ropeLength);
-		rope.GetStartOfRope().GetComponent<HingeJoint2D>().connectedBody = m_Tack.GetComponent<Rigidbody2D> ();
-
-        GameObject balloon = Instantiate(m_PrefabBalloune, new Vector2(x, 3), Quaternion.identity) as GameObject;
+		
+		GameObject balloon = Instantiate(m_PrefabBalloune, new Vector2(x, 3), Quaternion.identity) as GameObject;
 		DistanceJoint2D balloonJoint= balloon.GetComponent<DistanceJoint2D> ();
 		balloonJoint.connectedBody = m_Tack.GetComponent<Rigidbody2D>();
 
+		float ropeLength = balloonJoint.distance;
+		GameObject ropeGameObject = Instantiate (m_PrefabRope, new Vector2 (x, 3), Quaternion.identity) as GameObject;
+		Rope rope = ropeGameObject.GetComponent<Rope> ();
+		rope.createRope (ropeLength, m_NumberOfHinges);
+		rope.GetStartOfRope().GetComponent<HingeJoint2D>().connectedBody = m_Tack.GetComponent<Rigidbody2D> ();
+
 		//attach balloon to rope
-		balloon.GetComponent<HingeJoint2D> ().connectedBody = rope.GetEndOfRope().GetComponent<Rigidbody2D>();
+		HingeJoint2D balloonHinge = balloon.GetComponent<HingeJoint2D> ();
+		balloonHinge.connectedBody = rope.GetEndOfRope().GetComponent<Rigidbody2D>();
+		balloonHinge.connectedAnchor = new Vector2 (0, rope.GetLengthOfEachSegment());
 
 		//rope = balloon.AddComponent<Rope>();
 		//Debug.Log ("balloon ropes : " + balloon.GetComponents<Rope>().Length);
