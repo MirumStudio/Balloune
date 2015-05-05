025852d83d9872bed75a623ed06785c0b3021fd6
Vincent Bernier <vince14_evil@hotmail.com>
2015-05-05 13:10:37 -0400

    Change Sqlite version and create test

---
+++ Assets/Scripts/Radix/DatabaseManagement/SQLite/SqliteTest.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using Mono.Data.Sqlite;
+using System.Data;
+using System;
+
+public class SqliteTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string conn = "URI=file:" + Application.dataPath + "/Databases/test.db"; //Path to database.
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+        IDbCommand dbcmd = dbconn.CreateCommand();
+
+        string sqlQuery = "SELECT lol " + "FROM TestData";
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            int value = reader.GetInt32(0);
+
+            Debug.Log("lol= " + value);
+        }
+
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
