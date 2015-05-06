c83851df6c10373f184f08d432412e82cd9c2ecc
Vincent Bernier <vince14_evil@hotmail.com>
2015-05-06 06:56:27 -0400

    Create Sqlite Service and test

---
+++ Assets/Editor/UnityTestTools/Tests/Database/SqliteTest.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+
+public class SqliteTest 
+{
+    [Test]
+    public void lodgfgl()
+    {
+        Assert.Pass();
+    }
+}
---
+++ Assets/Scripts/Radix/DatabaseManagement/SQLite/SqliteService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Radix.Service;
+using UnityEngine;
+using System.Data;
+using Mono.Data.Sqlite;
+
+namespace Radix.DatabaseManagement.Sqlite
+{
+    public class SqliteService : ServiceBase
+    {
+        protected override void Init()
+        {
+
+        }
+
+        protected override void Dispose()
+        {
+
+        }
+
+        void Start()
+        {
+            string conn = "URI=file:" + Application.dataPath + "/Databases/test.db"; //Path to database.
+            IDbConnection dbconn;
+            dbconn = (IDbConnection)new SqliteConnection(conn);
+            dbconn.Open(); //Open connection to the database.
+            IDbCommand dbcmd = dbconn.CreateCommand();
+
+            string sqlQuery = "SELECT lol " + "FROM TestData";
+            dbcmd.CommandText = sqlQuery;
+            IDataReader reader = dbcmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int value = reader.GetInt32(0);
+
+                Debug.Log("lol= " + value);
+            }
+
+            reader.Close();
+            reader = null;
+            dbcmd.Dispose();
+            dbcmd = null;
+            dbconn.Close();
+            dbconn = null;
+        }
+
+        public bool ExecuteQuery(SQLQuery pQuery)
+        {
+            return true;
+        }
+    }
+}
