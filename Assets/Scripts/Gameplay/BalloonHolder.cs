e7f355fa334aba00473dc41e3a4986e8e1c4ebac
CourteousCrook <pocorbin@gmail.com>
2015-05-06 09:06:55 +0200

    Cleaned up Balloon Creation and Rope Creation

--- Assets/Scripts/Gameplay/BalloonHolder.cs
+++ Assets/Scripts/Gameplay/BalloonHolder.cs
@@ -20,41 +20,63 @@ public class BalloonHolder : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		int firstBalloonX = -45;
+		int firstBalloonX = -32;
 		for (int i = 0; i < m_LifeBalloons.Length; i++) {
-			m_LifeBalloons[i] = CreateBalloon (firstBalloonX - (i * 3));
-			BalloonBehavior balloonBehavior = m_LifeBalloons[i].GetComponent<BalloonBehavior>();
-			balloonBehavior.m_Parent = m_Tack.transform;
-			balloonBehavior.balloonIndex = i;
+			m_LifeBalloons[i] = CreateBalloon (firstBalloonX - (i * 3), 1.2f);
+			Rope newRope = CreateRopeForBalloon(m_LifeBalloons[i]);
+			AttachRope (m_LifeBalloons[i], newRope);
+			SetBalloonBehavior(m_LifeBalloons[i], i);
 			i = m_LifeBalloons.Length;
 		}
 	}
 
-    private GameObject CreateBalloon(float x)
+    private GameObject CreateBalloon(float x, float y)
 	{
-		//Rope rope = new Rope(ropeLength);
-		//attach rope to character
-		
-		GameObject balloon = Instantiate(m_PrefabBalloune, new Vector2(x, 3), Quaternion.identity) as GameObject;
-		DistanceJoint2D balloonJoint= balloon.GetComponent<DistanceJoint2D> ();
-		balloonJoint.connectedBody = m_Tack.GetComponent<Rigidbody2D>();
-
-		float ropeLength = balloonJoint.distance;
-		GameObject ropeGameObject = Instantiate (m_PrefabRope, new Vector2 (x, 3), Quaternion.identity) as GameObject;
-		Rope rope = ropeGameObject.GetComponent<Rope> ();
-		rope.createRope (ropeLength, m_NumberOfHinges);
-		rope.GetStartOfRope().GetComponent<HingeJoint2D>().connectedBody = m_Tack.GetComponent<Rigidbody2D> ();
-
-		//attach balloon to rope
-		HingeJoint2D balloonHinge = balloon.GetComponent<HingeJoint2D> ();
-		balloonHinge.connectedBody = rope.GetEndOfRope().GetComponent<Rigidbody2D>();
-		balloonHinge.connectedAnchor = new Vector2 (0, rope.GetLengthOfEachSegment());
-
-		//rope = balloon.AddComponent<Rope>();
-		//Debug.Log ("balloon ropes : " + balloon.GetComponents<Rope>().Length);
+		GameObject balloon = Instantiate(m_PrefabBalloune, new Vector2(x, y), Quaternion.identity) as GameObject;
 		heldBalloons++;
 		return balloon;
     }
+
+	private Rope CreateRopeForBalloon(GameObject pBalloon)
+	{
+		float maxBalloonDistance = SetMaxBalloonDistance (pBalloon);
+		GameObject ropeGameObject = Instantiate (m_PrefabRope, new Vector2 (pBalloon.transform.position.x, 3), Quaternion.identity) as GameObject;
+		Rope rope = ropeGameObject.GetComponent<Rope> ();
+		rope.createRope (maxBalloonDistance, m_NumberOfHinges);
+		return rope;
+	}
+
+	private float SetMaxBalloonDistance(GameObject pBalloon)
+	{
+		DistanceJoint2D balloonJoint= pBalloon.GetComponent<DistanceJoint2D> ();
+		balloonJoint.connectedBody = m_Tack.GetComponent<Rigidbody2D>();
+		return balloonJoint.distance;
+	}
+
+	private void AttachRope(GameObject pBalloon, Rope pRope)
+	{
+		AttachRopeToCharacter(pRope);
+		AttachRopeToBalloon(pBalloon, pRope);
+	}
+
+	private void AttachRopeToCharacter(Rope pRope)
+	{
+		pRope.GetStartOfRope().GetComponent<HingeJoint2D>().connectedBody = m_Tack.GetComponent<Rigidbody2D> ();
+	}
+
+	private void AttachRopeToBalloon(GameObject pBalloon, Rope pRope)
+	{
+		HingeJoint2D balloonHinge = pBalloon.GetComponent<HingeJoint2D> ();
+		balloonHinge.connectedBody = pRope.GetEndOfRope().GetComponent<Rigidbody2D>();
+		balloonHinge.connectedAnchor = new Vector2 (0, pRope.GetLengthOfEachSegment());
+	}
+
+	private void SetBalloonBehavior(GameObject pBalloon, int pBalloonIndex)
+	{
+		BalloonBehavior balloonBehavior = pBalloon.GetComponent<BalloonBehavior>();
+		balloonBehavior.m_Parent = m_Tack.transform;
+		balloonBehavior.balloonIndex = pBalloonIndex;
+	}
 
 	public void destroyBalloon(int pBalloonIndex) {
 		Destroy (m_LifeBalloons [pBalloonIndex]);
@@ -69,7 +91,7 @@ public class BalloonHolder : MonoBehaviour {
 	public int countBalloons() {
 		return heldBalloons;
 	}
-	// Update is called once per frame
+
 	void Update () {
 	
 	}
--- Assets/Scripts/Gameplay/Rope.cs
+++ Assets/Scripts/Gameplay/Rope.cs
@@ -4,6 +4,7 @@ public class Rope : MonoBehaviour
 {
 	private float mRopeLength;
 	private int mNumberOfHinges;
+	private float mLengthOfEachSegment;
 
 	private GameObject[] mRopeSegments;
 
@@ -12,33 +13,7 @@ public class Rope : MonoBehaviour
 
 	public Rope (int pRopeLength)
 	{
-		/*mRopeLength = pRopeLength;
-		mRopeSegments = new GameObject[mRopeLength];
 
-		for (int i = 0; i < mRopeLength; i++) {
-			GameObject newSegment = new GameObject();
-			Rigidbody2D newRigidBody = newSegment.AddComponent<Rigidbody2D>();
-			HingeJoint2D newHinge = newSegment.AddComponent<HingeJoint2D>();
-			LineRenderer newLineRenderer = newSegment.AddComponent<LineRenderer>();
-			//SpriteRenderer newSprite = newSegment.AddComponent<SpriteRenderer>();
-			newSegment.name = "hinge" + i;
-			newRigidBody.gravityScale = -1;
-			newRigidBody.drag = 1;
-			newLineRenderer.material = (Material)Resources.Load("Assets/Ressources/Rope/ropeMaterial.mat");
-			//newSprite.sortingOrder = 24;
-			//newSprite.sprite = UnityEngine.Sprite.Create(Resources.Load<Texture2D>("Ressources/balloonRope"), new Rect(0,0,10,100), new Vector2(0,0));
-			//newSprite.sprite = Resources.Load<Sprite>("balloonRope");
-			//newSprite.sprite = (Sprite) Resources.LoadAssetAtPath("Assets/Ressources/balloonRope", typeof(Sprite));
-			if (i > 0) {
-				newHinge.connectedBody = mRopeSegments[i - 1].GetComponent<Rigidbody2D>();
-				newHinge.connectedAnchor = new Vector2(0, 0.5f);
-			}
-			else {
-				newHinge.connectedAnchor = new Vector2(0,0);
-			}
-			newHinge.anchor = new Vector2(0, -0.5f);
-			mRopeSegments[i] = newSegment;
-		}*/
 	}
 
 	public void createRope(float pRopeLength, int pNumberOfHinges)
@@ -46,39 +21,42 @@ public class Rope : MonoBehaviour
 		mRopeLength = pRopeLength;
 		mNumberOfHinges = pNumberOfHinges;
 		mRopeSegments = new GameObject[mNumberOfHinges];
-		float lengthOfEachSegment = GetLengthOfEachSegment();
+		mLengthOfEachSegment = GetLengthOfEachSegment();
+		CreateRopeSegments ();
+		AttachRopeSegments ();
+	}
+
+	private void CreateRopeSegments()
+	{
 		for (int i = 0; i < mNumberOfHinges; i++) {
 			GameObject newSegment = Instantiate (m_RopeSegmentPrefab, new Vector2 (0, i), Quaternion.identity) as GameObject;
-			LineRenderer segmentLineRenderer = newSegment.GetComponent<LineRenderer>();
-			segmentLineRenderer.SetPosition (1, new Vector3(0, lengthOfEachSegment, -1));
-			HingeJoint2D segmentHinge = newSegment.GetComponent<HingeJoint2D>();
+			LineRenderer segmentLineRenderer = newSegment.GetComponent<LineRenderer> ();
+			segmentLineRenderer.SetPosition (1, new Vector3 (0, mLengthOfEachSegment, -1));
 			newSegment.name = "segment" + i;
+			
+			mRopeSegments [i] = newSegment;
+		}
+	}
+
+	private void AttachRopeSegments()
+	{
+		for (int i = 0; i < mRopeSegments.Length; i++) {
+			HingeJoint2D segmentHinge = mRopeSegments[i].GetComponent<HingeJoint2D> ();
 			if(i > 0)
 			{
 				GameObject previousSegment = mRopeSegments[i - 1];
-				segmentHinge.connectedBody = mRopeSegments[i - 1].GetComponent<Rigidbody2D>();
-				segmentHinge.connectedAnchor = new Vector2(0, lengthOfEachSegment);
+				segmentHinge.connectedBody = previousSegment.GetComponent<Rigidbody2D>();
+				segmentHinge.connectedAnchor = new Vector2(0, mLengthOfEachSegment);
 			}
 			else {
 				segmentHinge.connectedAnchor = new Vector2(0,0);
 			}
-
-			mRopeSegments[i] = newSegment;
 		}
 	}
-
+	
 	public float GetLengthOfEachSegment()
 	{
-		float returnValue = (mRopeLength / mNumberOfHinges) - 0.5f;
-		Debug.Log (mRopeLength);
-		Debug.Log (mNumberOfHinges);
-		Debug.Log (returnValue);
-		return returnValue;
-	}
-
-	public int GetLength()
-	{
-		return mNumberOfHinges;
+		return (mRopeLength / mNumberOfHinges) - 0.5f;
 	}
 
 	public GameObject GetStartOfRope()
