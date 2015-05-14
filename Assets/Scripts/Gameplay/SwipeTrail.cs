47524ef80333205b8b755da22814e0b589b072ef
Vincent Bernier <vince14_evil@hotmail.com>
2015-05-14 06:57:26 -0400

    Add work for gas Source. Big commit because I had a problem with Git

---
+++ Assets/Scripts/Gameplay/SwipeTrail.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwipeTrail : MonoBehaviour {
+
+    private TrailRenderer mTrailRenderer;
+    private float mTrailRendDefaultTime;
+    private float mDragEndTime;
+
+    void Start()
+    {
+        mTrailRenderer = GetComponent<TrailRenderer>();
+        mTrailRendDefaultTime = mTrailRenderer.time;
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Vector3 position;
+            position = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);//get position, where touch is detected
+            position.z = -1;
+
+            transform.position = position;
+
+            //wait little bit before showing trail renderer. used to not draw trail at first click/touch, 
+            //because this object jumps from last click/touch position to current click/touch position and it'll look weird
+            if (Time.time > mDragEndTime + 0.05f)
+            {
+                mTrailRenderer.time = mTrailRendDefaultTime;
+            }
+        }
+        else
+        {
+            mTrailRenderer.time = 0;
+            mDragEndTime = Time.time;
+        }
+    }
+}
