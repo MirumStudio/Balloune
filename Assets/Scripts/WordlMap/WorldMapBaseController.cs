c59ed2b0e640b2902153566635844f91a203c025
Vincent Bernier <vince14_evil@hotmail.com>
2015-05-27 09:01:24 -0400

    Set world map with good Level Add level 1 to 4 Crete LevelController.prefab

--- Assets/Scripts/WordlMap/WorldMapBaseController.cs
+++ Assets/Scripts/WordlMap/WorldMapBaseController.cs
@@ -14,8 +14,8 @@ public class WorldMapBaseController : MonoBehaviour {
     public void OnPlayClick()
     {
 		//TODO: Remove when other level will be created Maybe, we can create a fallback for demo purpose
-        //Application.LoadLevel("Level" + m_Id + "_" + (GetComponentInChildren<WordlMapCharacter>().CurrentLevel+1));
-        Application.LoadLevel("Level" + m_Id + "_" + 1);
+        Application.LoadLevel("Level" + m_Id + "_" + (GetComponentInChildren<WordlMapCharacter>().CurrentLevel+1));
+        //Application.LoadLevel("Level" + m_Id + "_" + 1);
     }
 
     void OnDestroy()
