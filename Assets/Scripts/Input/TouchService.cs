e1a669f31fff3d8082075747a13dd6ee68e69d5d
Vincent Bernier <vince14_evil@hotmail.com>
2015-05-29 07:31:36 -0400

    Add empty class for touch management

---
+++ Assets/Scripts/Input/TouchService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#if UNITY_IOS
+public class TouchService : iOSTouchService
+#else
+public class TouchService : DefaultTouchService
+#endif
+{
+}
---
+++ Assets/Scripts/Input/TouchServiceBase.cs
@@ -0,0 +1,25 @@
+﻿using Radix.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class TouchServiceBase : MonoBehaviour
+{
+  /*  protected override void Init()
+    {
+        //throw new NotImplementedException();
+    }
+
+    protected override void Dispose()
+    {
+        //throw new NotImplementedException();
+    }*/
+
+    protected virtual void Update()
+    {
+
+    }
+}
+
