d09847756f8a9c12cf82bc9ce44873f304c72547
Vincent Bernier <vince14_evil@hotmail.com>
2015-05-29 07:19:36 -0400

    Add Debug text Component

---
+++ Assets/Scripts/Utilities/DebugText.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DebugText : MonoBehaviour {
+
+    private static Text Instance;
+
+	void Start () {
+        Instance = GetComponent<Text>();
+	}
+
+    public static void Log(string pMessage)
+    {
+        Log(pMessage, Color.black);
+    }
+
+    public static void Log(string pMessage, Color pColor)
+    {
+        if (Instance != null)
+        {
+            Instance.text = pMessage;
+            Instance.color = pColor;
+        }
+    }
+}
