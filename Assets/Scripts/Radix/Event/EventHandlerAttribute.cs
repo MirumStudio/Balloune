8f67907e719f1b2043c85bd6b031d178897f027c
Vincent Bernier <vince14_evil@hotmail.com>
2015-06-10 10:15:08 -0400

    Add EventHandlerAttribute

---
+++ Assets/Scripts/Radix/Event/EventHandlerAttribute.cs
@@ -0,0 +1,39 @@
+﻿/* -----      MIRUM STUDIO      -----
+ * Copyright (c) 2015 All Rights Reserved.
+ * 
+ * This source is subject to a copyright license.
+ * For more information, please see the 'LICENSE.txt', which is part of this source code package.
+ */
+
+using Radix.ErrorMangement;
+using System;
+#if UNITY_WSA || UNITY_WP8 || UNITY_WP8_1
+using System.Reflection;
+#endif
+namespace Radix.Event
+{
+    public class EventHandlerAttribute : Attribute
+    {
+        public EventHandlerAttribute()
+        {
+            Handler = typeof(VoidDelegate);
+        }
+
+        public EventHandlerAttribute(Type pHandler)
+        {
+#if UNITY_WSA || UNITY_WP8 || UNITY_WP8_1
+            if (pHandler.GetTypeInfo().IsSubclassOf(typeof(Delegate)))
+#else
+            if (pHandler.IsSubclassOf(typeof(Delegate)))
+#endif
+            {
+                Handler = pHandler;
+            }
+            else
+            {
+                Error.Create("Event attribute is not a delegate", EErrorSeverity.MAJOR);
+            }
+        }
+        public Type Handler { get; private set; }
+    }
+}
