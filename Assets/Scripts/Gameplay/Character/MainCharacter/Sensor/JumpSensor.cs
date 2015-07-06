a0b3bae171de0cc9fa2902948c1a388c08d32af3
Vincent bernier <vince14_evil@hotmail.com>
2015-07-06 11:51:07 -0400

    Make automatic jump for the min character

---
+++ Assets/Scripts/Gameplay/Character/MainCharacter/Sensor/JumpSensor.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System.Collections;
+using Radix.Event;
+
+public class JumpSensor : CharacterSensor {
+
+	private const string GROUND_LAYER_NAME = "Ground"; 
+
+	private const string IS_PULlING_PARAMTER = "IsPulling";
+	private const string SPEED_PARAMTER = "Speed";
+
+	private float mWidth;
+	private float mHeight;
+	
+	Collider2D mCollider;
+
+	private float AJUST_X = 1f;
+
+	protected override void Start ()
+	{
+		base.Start ();
+
+		mCollider = GetComponent<Collider2D>();
+		mWidth = mCollider.bounds.size.x;
+		mHeight = mCollider.bounds.size.y;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(mAnimator.GetCurrentAnimatorStateInfo(0).IsName("Moving"))
+		{
+			float speed = mAnimator.GetFloat(SPEED_PARAMTER);
+
+			Vector2 bottom;
+			Vector2 top;
+
+			if(speed < 0)
+			{
+				//CheckLeft();
+				bottom = GetBottomLeftCorner();
+				top = GetTopLeftCorner();
+				bottom.x -= AJUST_X;
+				top.x -= AJUST_X;
+
+				Check(bottom, top);
+
+			}
+			else if(speed > 0)
+			{
+				bottom = GetBottomRightCorner();
+				top = GetTopRightCorner();
+				bottom.x += AJUST_X;
+				top.x += AJUST_X;
+				
+				Check(bottom, top);
+			}
+		}
+		
+	}
+
+	private void CheckRight()
+	{
+
+	}
+
+	private void CheckLeft()
+	{
+		Vector2 vector = GetBottomLeftCorner();
+		vector.y += 0.05f;
+		Debug.DrawLine(GetTopLeftCorner(), vector, Color.green);
+		Physics2D.Linecast(GetTopLeftCorner(), vector, GroundLayerMask);
+	}
+
+	private void Check(Vector2 mBottom, Vector2 mTop)
+	{
+		mBottom.y += 0.2f;
+		Debug.DrawLine (mTop, mBottom, Color.green);
+		if(Physics2D.Linecast(mTop, mBottom, GroundLayerMask))
+		{
+			Debug.Log("DETECT");
+			mAnimator.SetBool("HaveToJump", true);
+		}
+	}
+
+	#region GetInformation
+	private Vector2 GetTopRightCorner()
+	{
+		return new Vector2(GetRightEdge(), GetTopEdge());
+	}
+	
+	private Vector2 GetTopLeftCorner()
+	{
+		return new Vector2(GetLeftEdge(), GetTopEdge());
+	}
+	
+	private Vector2 GetBottomRightCorner()
+	{
+		return new Vector2(GetRightEdge(), GetBottomEdge());
+	}
+	
+	private Vector2 GetBottomLeftCorner()
+	{
+		return new Vector2(GetLeftEdge(), GetBottomEdge());
+	}
+	
+	private float GetTopEdge()
+	{
+		return GetCenter().y + mHeight / 2;
+	}
+	
+	private float GetBottomEdge()
+	{
+		return GetCenter().y - mHeight / 2 - 0.05f;
+	}
+	
+	private float GetLeftEdge()
+	{
+		return GetCenter().x - mWidth / 2 - 0.1f;
+	}
+	
+	private float GetRightEdge()
+	{
+		return GetCenter().x + mWidth / 2 + 0.1f;
+	}
+	
+	private Vector2 GetCenter()
+	{
+		return mCollider.bounds.center;
+	}
+	
+	private int GroundLayerMask
+	{
+		get
+		{
+			return 1 << LayerMask.NameToLayer(GROUND_LAYER_NAME);
+		}
+	}
+
+	#endregion
+}
+
--- Assets/Scripts/Gameplay/Character/MainCharacter/State/JumpingState.cs
+++ Assets/Scripts/Gameplay/Character/MainCharacter/State/JumpingState.cs
@@ -2,4 +2,18 @@
 using System.Collections;
 
 public class JumpingState : CharacterState {
+	[SerializeField]
+	protected float m_JumpForce = 850f;
+
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		base.OnStateEnter (animator, stateInfo, layerIndex);
+		mBody.AddForce(new Vector2(0f, m_JumpForce));
+		Debug.Log ("lol");
+		animator.SetBool ("HaveToJump", false);
+
+	}
+
+	override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+
+	}
 }
