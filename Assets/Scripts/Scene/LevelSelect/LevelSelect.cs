367202557c86629871771add039a37489543ed74
Vincent bernier <vince14_evil@hotmail.com>
2015-07-28 15:07:39 -0400

    Change Title of level in level select

--- Assets/Scripts/Scene/LevelSelect/LevelSelect.cs
+++ Assets/Scripts/Scene/LevelSelect/LevelSelect.cs
@@ -115,70 +115,70 @@ public class LevelSelect : BaseView {
         UILevelInfo level1 = new UILevelInfo();
         level1.Index = 1;
         level1.Chapter = 1;
-        level1.Name = "Tittle1";
+        level1.Name = "Title1";
         level1.IsUnlocked = true;
         mLevels.Add(level1);
 
         UILevelInfo level2 = new UILevelInfo();
         level2.Index = 2;
         level2.Chapter = 1;
-        level2.Name = "Tittle2";
+        level2.Name = "Title2";
         level2.IsUnlocked = true;
         mLevels.Add(level2);
 
         UILevelInfo level3 = new UILevelInfo();
         level3.Index = 3;
         level3.Chapter = 1;
-        level3.Name = "Tittle3";
+        level3.Name = "Title3";
         level3.IsUnlocked = true;
         mLevels.Add(level3);
 
         UILevelInfo level4 = new UILevelInfo();
         level4.Index = 4;
         level4.Chapter = 1;
-        level4.Name = "Tittle4";
+        level4.Name = "Title4";
         level4.IsUnlocked = true;
         mLevels.Add(level4);
 
         UILevelInfo level5 = new UILevelInfo();
         level5.Index = 5;
         level5.Chapter = 1;
-        level5.Name = "Tittle5";
+        level5.Name = "Title5";
         level5.IsUnlocked = true;
         mLevels.Add(level5);
 
         UILevelInfo level6 = new UILevelInfo();
         level6.Index = 6;
         level6.Chapter = 1;
-        level6.Name = "Tittle6";
+        level6.Name = "Title6";
         level6.IsUnlocked = true;
         mLevels.Add(level6);
 
         UILevelInfo level7 = new UILevelInfo();
         level7.Index = 7;
         level7.Chapter = 1;
-        level7.Name = "Tittle7";
-        level7.IsUnlocked = false;
+        level7.Name = "Title7";
+        level7.IsUnlocked = true;
         mLevels.Add(level7);
 
         UILevelInfo level8 = new UILevelInfo();
         level8.Index = 8;
         level8.Chapter = 1;
-        level8.Name = "Tittle8";
-        level8.IsUnlocked = false;
+        level8.Name = "Title8";
+        level8.IsUnlocked = true;
         mLevels.Add(level8);
 
         UILevelInfo level9 = new UILevelInfo();
         level9.Index = 9;
         level9.Chapter = 1;
-        level9.Name = "Tittle9";
+        level9.Name = "Title9";
         level9.IsUnlocked = false;
         mLevels.Add(level9);
 
         UILevelInfo level10 = new UILevelInfo();
         level10.Index = 10;
         level10.Chapter = 1;
-        level10.Name = "Tittle10";
+        level10.Name = "Title10";
         level10.IsUnlocked = false;
         mLevels.Add(level10);
     }
