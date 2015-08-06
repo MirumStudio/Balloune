ead46d9973b3503ece24183ff530ed3fcfd8aef0
CourteousCrook <pocorbin@gmail.com>
2015-08-06 16:47:14 -0400

    balloons now detach immediately when they reach their maximum distance

--- Assets/Scripts/Gameplay/Balloon/BalloonPhysics.cs
+++ Assets/Scripts/Gameplay/Balloon/BalloonPhysics.cs
@@ -16,7 +16,7 @@ public class BalloonPhysics : MonoBehaviour
 	private const int PICKEDUP_BALLOON_COLLISION_LAYER = 10;
 
 	private const float MAX_DRAG_VELOCITY = 15f;
-	private const float TIME_TO_DETACH = 0.35f;
+	private const float TIME_TO_DETACH = 0f;
 	
 	[SerializeField]
     public Transform m_Parent = null;
@@ -88,10 +88,6 @@ public class BalloonPhysics : MonoBehaviour
 				DragBalloon(touchPosition, currentBalloonPosition);
 				mBalloon.OnMove(balloonDistance);
 			}
-			if (IsBalloonAtMaximumDistance())
-			{
-				SetVelocity(Vector2.zero);
-			}
         }
     }
 
@@ -124,7 +120,7 @@ public class BalloonPhysics : MonoBehaviour
 
 	public void DetachBalloon()
 	{
-		if (mTimePullingAtMaximumDistance >= TIME_TO_DETACH) {
+		if (mIsTouched && mBalloonHolder != null && IsBalloonAtMaximumDistance()) {
 			mDistanceJoint.enabled = false;
 			mBalloonJoint.enabled = false;
 			mLineRenderer.enabled = false;
