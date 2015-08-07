96e02dafbb9ac52792e8c69022292079e7ea6a3e
Vincent bernier <vince14_evil@hotmail.com>
2015-08-07 09:19:20 -0400

    Add attack state to the fury mnster

---
+++ Assets/Scripts/Gameplay/Character/FuryCharacterController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FuryCharacterController : MonoBehaviour {
+
+    private const string FURY_PARAMATER = "IsInFury";
+
+    protected Animator mAnimator;
+    protected SpriteRenderer mRenderer;
+
+    private bool mIsRed = false;
+
+    virtual protected void Start () {
+        mAnimator = GetComponent<Animator> ();
+        mRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	void Update () {
+        UpdateColor();
+	}
+
+    private void UpdateColor()
+    {
+        if (!mIsRed && mAnimator.GetBool(FURY_PARAMATER))
+        {
+            mRenderer.color = Color.red;
+        } else if(mIsRed)
+        {
+            mRenderer.color = Color.white;
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/Character/Sensor/FuryBalloonSensor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Radix.Logging;
+
+public class FuryBalloonSensor : CharacterSensor {
+
+    protected const string GIRL_BALLOON_LAYER_NAME = "GirlBalloon"; 
+    protected const string NOT_GIRL_BALLOON_LAYER_NAME = "NotGirlBalloon";
+
+    private const string BALLOON_IDENTIFIER = "Balloon";
+	
+    private const string FURY_PARAMATER = "IsInFury";
+
+    [SerializeField]
+    private float m_SensorRange = 3.0f;
+
+	void Update () {
+        // (!mAnimator.GetBool(FURY_PARAMATER))
+        {
+            Collider2D[] touchedColliders = Physics2D.OverlapCircleAll(transform.position, m_SensorRange, BalloonLayerMask);
+
+            foreach (Collider2D collider2d in touchedColliders)
+            { 
+                RaycastHit2D hit = Physics2D.Linecast(transform.position, collider2d.transform.position, GroundLayerMask);
+
+                mAnimator.SetBool(FURY_PARAMATER,!hit);
+            }
+        }
+	}
+
+    protected int BalloonLayerMask
+    {
+        get
+        {
+            return (1 << LayerMask.NameToLayer(GIRL_BALLOON_LAYER_NAME)) | (1 << LayerMask.NameToLayer(NOT_GIRL_BALLOON_LAYER_NAME));
+        }
+    }
+}
